4ad2fe20d8e87d9f6ca7fe76731ab1b48c4dee9b
hpeyton33 <hpeyton33@gmail.com>
2025-12-23 15:22:00 -0600

    creat recursive fib function

--- js.js
+++ js.js
@@ -1,4 +1,4 @@
- function InitFibFunction (usernumber) {
+function InitFibFunction (usernumber) {
 const outputArray = []    
 for (let index = 0; index < usernumber; index++) {
 if (outputArray.length < 1) {
@@ -11,4 +11,25 @@ outputArray.push(fib)
 }
 console.log(outputArray)
 }
+
+function recursiveFibFunction(usernumber,outputArray=[],index=0){
+      
+if (usernumber == 0) {
+console.log(outputArray)    
+} else {
+if (outputArray.length == 0) {
+const fib =  outputArray[index - 2] + outputArray[index - 1] || 0   
+outputArray.push(fib)
+index++
+recursiveFibFunction(usernumber-1,outputArray,index)   
+} else {
+const fib =  outputArray[index - 2] + outputArray[index - 1] || 1   
+outputArray.push(fib)
+index++
+recursiveFibFunction(usernumber-1,outputArray,index)   
+}    
+}
+}
+
 InitFibFunction(20)
+recursiveFibFunction(20)
